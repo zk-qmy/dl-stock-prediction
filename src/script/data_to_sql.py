a2b6e00101b56b3c53d6ddc47b62f34b2cb51121
My <ltquemy@gmail.com>
2024-09-30 13:31:29 +0700

    ignore coderunner

--- src/script/data_to_sql.py
+++ src/script/data_to_sql.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import os
-from sqlalchemy import create_engine
 import yaml
+from utils import db_connection
 
 # pip install SQLAlchemy
-
+# pip install pyyaml
 # install MySQL Driver
 # pip install PyMySQL
 
+'''
 # Load configuration
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
@@ -15,23 +16,38 @@ with open('config.yaml', 'r') as file:
 # Database connection setup
 db_config = config['database']
 engine = create_engine(f"mysql+pymysql://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['dbname']}")
+'''
 
+config = db_connection.load_config()
+engine = db_connection.get_db_engine(config)
 # Path to the folder containing your CSV files
 folder_path = config['data']['folder_path']
 
-# Loop through all CSV files in the folder and combine them
-for file in os.listdir(folder_path):
-    if file.endswith('.csv'):
-        df = pd.read_csv(os.path.join(folder_path, file))
-        company_name = file.split('.')[0]
-        df['company_name'] = company_name
-        df['Date'] = pd.to_datetime(df['Date'])
-        df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 
-                           'Low': 'low', 'Close': 'close', 'Volume': 'volume'}, inplace=True)
-        df.to_sql('stocks', con=engine, if_exists='append', index=False)
-        print(f"Inserted data for {company_name} into the database.")
-print("Data insertion complete.")
 
+def
+# Loop through all CSV files in the folder and combine them
+try:
+    for file in os.listdir(folder_path):
+        if file.endswith('.csv'):
+            try:
+                df = pd.read_csv(os.path.join(folder_path, file))
+                company_ticker = file.split('-')[0]
+                df['Ticker'] = company_ticker
+                df['TradingDate'] = pd.to_TradingDatetime(df['TradingDate'])
+                df.rename(columns={'TradingDate': 'tradingdate', 'Open': 'open',
+                                   'High': 'high', 'Low': 'low', 'Close': 'close',
+                                   'Volume': 'volume'}, inplace=True)
+                df.to_sql('stockdb', con=engine,
+                          if_exists='append', index=False)
+                print(f"Inserted data for {company_ticker} into the database.")
+            except Exception as e:
+                print(f'Error: {e}')
+    print("Data insertion complete.")
+except FileNotFoundError as e:
+    print(f'File not found: {e}')
+
+
+'''
 import mysql.connector
 connection = mysql.connector.connect(
     user="root",
@@ -41,7 +57,7 @@ connection = mysql.connector.connect(
     ssl_disablrd=True
 )
 cursor = connection.cursor()
-query = '''select * from open;'''
+query = select * from open;
 cursor.excecute(query)
 results = []
 for i, data in enumerate(cursor):
@@ -49,3 +65,4 @@ for i, data in enumerate(cursor):
 cursor.close()
 connection.close()
 df = pd.DataFrame(results)
+'''
--- src/utils/db_connection.py
+++ src/utils/db_connection.py
@@ -1,9 +1,37 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
 import yaml
+import mysql.connector
 
 
-def get_db_engine():
-    with open('config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
+def load_config():
+    try:
+        with open('config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
+            return config
+    except FileNotFoundError:
+        print("Error config.yaml not found")
+        exit(1)
+    except yaml.YAMLError as e:
+        print(f"Error reading file: {e}")
+        exit(1)
+
+
+def get_db_engine(config):
+    # Setup DB connection
     db_config = config['database']
-    return create_engine(f"mysql+pymysql://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['dbname']}")
+    try:
+        engine = create_engine(f"mysql+pymysql://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['dbname']}")
+        return engine
+    except exc.SQLAlchemyError as e:
+        print(f"Database Connecting failed: {e}")
+        exit(1)
+
+
+def connect_to_db(config):
+    mysql.connector.connect(
+        user = config['database']['username'],
+        password =config['database']['password'],
+        host= config['database']['host'],
+        database= config['database']['dbName'],
+        ssl_disablrd=True
+        )
