e067f28266d842af641b2d2b549b7063d9401a1f
My <ltquemy@gmail.com>
2024-10-01 21:36:03 +0700

    commit data_to_sql function

--- src/preprocessing/data_to_sql.py
+++ src/preprocessing/data_to_sql.py
@@ -14,14 +14,14 @@ def insert_to_sql(folder_path, engine, table_name):
             if file.lower().endswith('.csv'):
                 try:
                     df = pd.read_csv(os.path.join(folder_path, file))
-                    company_ticker = file.split('-')[0]
+                    company_ticker = file.split('.')[0].split('-')[0]
                     df['Ticker'] = company_ticker
                     try:
                         if "TradingDate" in df.columns:
-                            df['Date'] = pd.to_datetime(df['TradingDate'],
+                            df['date'] = pd.to_datetime(df['TradingDate'],
                                                         dayfirst=True)
                         elif "Date" in df.columns:
-                            df["Date"] = pd.to_datetime(df["Date"],
+                            df["date"] = pd.to_datetime(df["Date"],
                                                         dayfirst=True)
                         else:
                             print(f"Column Date not found {file}, skip")
@@ -35,18 +35,23 @@ def insert_to_sql(folder_path, engine, table_name):
                                        'Open': 'open', 'High': 'high',
                                        'Low': 'low', 'Close': 'close',
                                        'Volume': 'volume',
-                                       'AdjustedClose': 'adjustedclose'},
+                                       'Adjusted Close': 'adjustedclose'},
                               inplace=True)
+                    new_column_order = ['date', 'ticker', 'low', 'open',
+                                        'volume', 'high', 'close',
+                                        'adjustedclose']
+                    df = df[new_column_order]
                     df.to_sql(table_name, con=engine,
                               if_exists='append', index=False)
                     print(
                         f"Inserted data for {company_ticker} into database.")
-                except Exception as e:
-                    print(f'Error: {e}')
+                except Exception:
+                    # print(f'Error: {e}')
+                    raise
         print("Data insertion complete.")
-    except FileNotFoundError as e:
-        print(f'File not found: {e}')
-
+    except FileNotFoundError:
+        # print(f'File not found: {e}')
+        raise
 
 '''
 import mysql.connector
