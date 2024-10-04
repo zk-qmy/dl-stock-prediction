c7a3a63fa068c3bb564c4c4eb77b2c8604919333
My <ltquemy@gmail.com>
2024-10-04 18:32:49 +0700

    commit get_data

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 from utils.db_connection import load_config, get_db_engine, get_db_connection
 '''Encounter Wildcard imports when trying to
 from utils.db_connection import * '''
-from preprocessing.data_to_sql import insert_to_sql
+from preprocessing.data_to_sql import insert_to_sql, get_data
 import pandas as pd
 
 
@@ -13,15 +13,6 @@ folder_path = config['data']['test_path']
 db_name = config["database"]["dbname"]
 table_name = config["tables"]["testnasdag"]
 insert_to_sql(folder_path, engine, db_name, table_name)
-'''
-# Perform preprocessing (get data of oldest company) on sql
-connection = get_db_connection()
-cursor = connection.cursor()
-cursor.Execute(query)
-results = []
-for i, data in enumerate(cursor):
-    results.append(data)
-cursor.close()
-connection.close()
-df = pd.DataFrame(results)
-'''
+with engine.connect() as connection:
+    df = get_data(connection, db_name, table_name)
+print(df)
--- src/preprocessing/data_to_sql.py
+++ src/preprocessing/data_to_sql.py
@@ -8,22 +8,40 @@ from sqlalchemy import text
 # pip install PyMySQL
 
 
-def get_data():
-    ''' Get the dataset of the oldest company'''
+def get_data(connection, db_name, table_name):
+    ''' Get the dataset of the company with longest duration'''
+    query = f"""
+            SELECT *
+            FROM {db_name}.{table_name}
+            GROUP BY ticker
+            ORDER BY (MAX(date) - MIN(date)) DESC
+            LIMIT 1;
+        """
+    try:
+        # Convert the selected data to df
+        df = pd.read_sql_query(query, connection)
+        if not df.empty:
+            return df
+        else:
+            print('Data not found!')
+    except Exception:
+        raise
 
 
 def insert_to_sql(folder_path, engine, db_name, table_name):
     '''Insert all data from csv files to a database'''
     # Loop through all CSV files in the folder and combine them
     try:
-        with engine.begin() as connection:
+        with engine.begin() as connection:  # Begin transaction
             for file in os.listdir(folder_path):
                 if file.lower().endswith('.csv'):
                     try:
                         df = pd.read_csv(os.path.join(folder_path, file))
+                        # Create a column of ticker name from filename
                         company_ticker = file.split('.')[0].split('-')[0]
                         df['Ticker'] = company_ticker
                         try:
+                            # Convert date data
                             if "TradingDate" in df.columns:
                                 df['date'] = pd.to_datetime(df['TradingDate'],
                                                             dayfirst=True)
@@ -36,7 +54,7 @@ def insert_to_sql(folder_path, engine, db_name, table_name):
                         except Exception as e:
                             print(f"Error converting Date for {file}: {e}")
                             raise
-
+                        # Rename columns to align with the table in SQL DB
                         df.rename(columns={'TradingDate': 'date',
                                            'Ticker': 'ticker',
                                            'Open': 'open', 'High': 'high',
@@ -48,28 +66,14 @@ def insert_to_sql(folder_path, engine, db_name, table_name):
                                             'volume', 'high', 'close',
                                             'adjustedclose']
                         df = df[new_column_order]
+                        # Insert data to SQL database
                         df.to_sql(table_name, con=connection,
                                   if_exists='append', index=False)
                         print(
                             f"Inserted data for {company_ticker} into DB.")
                     except Exception:
-                        # print(f'Error: {e}')
                         raise
             print("Data insertion complete.")
-            query = f"""
-                SELECT ticker
-                FROM {db_name}.{table_name}
-                WHERE date = (SELECT MIN(date) FROM {db_name}.{table_name} );
-            """
-            try:
-                result = connection.execute(text(query))
-                tickers = result.fetchall()
-                if tickers:
-                    print(tickers)
-                else:
-                    print('sdjfnsnot found')
-            except Exception:
-                raise
     except FileNotFoundError:
-        # print(f'File not found: {e}')
         raise
+
