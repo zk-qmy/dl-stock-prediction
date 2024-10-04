9bc3cb0dc406f4a0cb7f95ee8aa47c55c8711b26
My <ltquemy@gmail.com>
2024-10-04 23:50:54 +0700

    modify notebook

--- src/main.py
+++ src/main.py
@@ -3,15 +3,27 @@ from utils.db_connection import load_config, get_db_engine
 '''Encounter Wildcard imports when trying to
 from utils.db_connection import * '''
 from preprocessing.data_to_sql import insert_to_sql, get_data
-
+import os
 
 config = load_config()
 engine = get_db_engine(config)
-# Path to the folder containing your CSV files
+# Path to the folder containing CSV files
 folder_path = config['data']['test_path']
 db_name = config["database"]["dbname"]
 table_name = config["tables"]["testnasdag"]
+
 insert_to_sql(folder_path, engine, db_name, table_name)
 with engine.connect() as connection:
     df = get_data(connection, db_name, table_name)
 print(df)
+
+
+# Specify the directory
+directory = 'data\\toprocess'
+
+# Create directory if it does not exist
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Save DataFrame as CSV
+df.to_csv(os.path.join(directory, 'nasdag_chosen.csv'), index=False)
