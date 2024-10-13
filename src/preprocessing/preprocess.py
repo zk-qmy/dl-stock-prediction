1f26db685dd29d02261b65af5962bbed7a12e61f
My <ltquemy@gmail.com>
2024-10-13 21:32:25 +0700

    commit get_samples for k days

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -22,10 +22,10 @@ def split_data(X_data, y_data, test_size=0.2, val_size=0.2):
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 
-def get_data_samples(df, kth_day,
-                     feature_slice,
-                     window_size=30,
-                     label_colID=3): 
+def get_data_samples_kth_day(df, kth_day,
+                             feature_slice,
+                             window_size=30,
+                             label_colID=3):
     # Split the dataset into time windows to get data samples.
     '''Predict label value of the next kth day based on multiple feature
     params:
@@ -52,6 +52,44 @@ def get_data_samples(df, kth_day,
 
         # Next value is the label (price of the next day) to be predicted
         data_label.append(df.iloc[i + window_size + kth_day, label_colID])
+
+        # Append new data sample (feature and label) to X_data and y_data
+        X_data.append(np.array(data_feature).reshape(window_size, 6))
+        y_data.append(np.array(data_label))
+    return X_data, y_data
+
+
+def get_data_samples_ks_day(df, k_days_ahead,
+                            feature_slice,
+                            window_size=30,
+                            label_colID=3):
+    # Split the dataset into time windows to get data samples.
+    '''Predict label value of the next kth day based on multiple feature
+    params:
+        df: dataframe
+        k_days_ahead: predict for the kth day
+        feature_slice: the slice of the features,
+            i.e, slice(a,b) for multi-feature
+            i.e, a for one feature, with a is the index of the label column
+        window_size: the number of data points considered at a time
+        label_colID: the column index of the label
+       '''
+    X_data = []
+    y_data = []
+
+    # Get the low, open, volume, high, close, adjustedclose
+    for i in range(0, len(df) - window_size - k_days_ahead):
+        data_feature = []
+        data_label = []
+
+        # Get a window_size time frame for data feature
+        for j in range(window_size):
+            data_feature.append(
+                df.iloc[i + j, feature_slice].to_numpy().tolist())
+
+        # Next value is the label (price of the next day) to be predicted
+        data_label.append(
+            df.iloc[(i+window_size):(i+window_size+k_days_ahead), label_colID])
 
         # Append new data sample (feature and label) to X_data and y_data
         X_data.append(np.array(data_feature).reshape(window_size, 6))
