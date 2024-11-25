d3c414870ac10cecf85345b4365a4240b240b4ee
My <ltquemy@gmail.com>
2024-11-25 18:59:49 +0700

    [deploy] commit ui

--- deployment/app.py
+++ deployment/app.py
@@ -4,6 +4,7 @@ import joblib
 import os
 import numpy as np
 import tensorflow as tf
+import charts
 
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
@@ -32,13 +33,19 @@ selected_ticker = st.selectbox("Select ticker", tickers)
 
 
 @st.cache_data  # to not to run the code again
-def get_30_latest_data(ticker):
+def get_dataFrame(ticker):
     data_file_path = os.path.join(data_folder_path,
                                   selected_ticker + "-History.csv")
     data = pd.read_csv(data_file_path)
     data.drop(columns="Unnamed: 0", inplace=True)
-    data.drop(columns='TradingDate', inplace=True)
+    # data.drop(columns='TradingDate', inplace=True)
+    data["TradingDate"] = pd.to_datetime(data["TradingDate"])
+    data.set_index("TradingDate", inplace=True)
     data.dropna()
+    return data
+
+
+def get_30_latest_data(data):
     latest_data_feature = np.array(data['Close'].tail(30)).reshape(1, 30)
     return latest_data_feature
 
@@ -74,9 +81,37 @@ def get_signal(y_pred):
     return "Hold", changes
 
 
-def predict_signal(ticker, model_path, feature_scalers_path, y_scaler_path):
+def get_signal_date_n_price(y_pred_denorm, df):
+    # Flatten the 2D array to 1D if it's not already 1D
+    # Use with cautious!!!
+    y_pred_flat = y_pred_denorm.flatten()
+
+    # Ensure future_dates has the correct number of dates (look_ahead is 7)
+    look_ahead = 7
+    last_date = df.index[-1]  # The last date in the historical data
+    # Generate the future dates (7 days ahead, including weekends)
+    future_dates = pd.date_range(
+        start=last_date, periods=look_ahead + 1, freq='D')[1:]  # Exclude the start date
+    # Create a pandas Series to map the predicted values to the corresponding dates
+    future_prices = pd.Series(y_pred_flat, index=future_dates)
+    # Get the index of the best (highest) predicted return
+    # Find the index of the best predicted return
+    best_buy_day_index = np.argmax(y_pred_flat)
+    best_sell_day_index = np.argmin(y_pred_flat)  # Find index of lowest price
+    # Get the best buy date (the date corresponding to the highest predicted return)
+    best_buy_date = future_dates[best_buy_day_index]
+    best_sell_date = future_dates[best_sell_day_index]
+    # Get the price for the best buy date
+    price_on_best_buy_day = future_prices[best_buy_date]
+    price_on_best_sell_day = future_prices[best_sell_date]
+
+    return best_buy_date, price_on_best_buy_day, best_sell_date, price_on_best_sell_day
+
+
+def predict_buying_date(ticker, model_path, feature_scalers_path, y_scaler_path):
+    data = get_dataFrame(ticker)
     # Get the latest data for the selected ticker
-    latest_data_feature = get_30_latest_data(ticker)
+    latest_data_feature = get_30_latest_data(data)
 
     # Normalize the data using the feature scaler
     data_norm = normalise(latest_data_feature, feature_scalers_path)
@@ -89,14 +124,15 @@ def predict_signal(ticker, model_path, feature_scalers_path, y_scaler_path):
     y_pred_denorm = y_scaler.inverse_transform(y_pred)
 
     # Get the buying/selling signal
-    signal_n_changes = get_signal(y_pred_denorm)
-    return signal_n_changes
+    # signal_n_changes = get_signal(y_pred_denorm)
+    buy_date = get_signal_date_n_price(y_pred_denorm, data)
+    return buy_date
 
 
 # Streamlit UI setup
 data_load_status = st.empty()
 data_load_status.text("Loading data....")
-output_data = predict_signal(
+output_data = predict_buying_date(
     selected_ticker, model_path, feature_scalers_path, y_scaler_path)
 
 # Display prediction
@@ -106,5 +142,24 @@ if output_data is None:
 else:
     data_load_status.empty()  # clear the loading status
     st.success("Predicted successfully!")
-    st.write(f"Signal: {output_data[0]}")
-    st.write(f"Close price changes in 7 days: {output_data[1]}")
+# Display Buy and Sell details
+st.subheader("Here is the buying/selling signal for the next week:")
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("#### 🟢 **Buy Information**")
+    st.markdown(f"**Buy Date**: _{output_data[0].strftime('%Y-%m-%d')}_")
+    st.markdown(f"**Buy Price**: _${output_data[1]:,.2f}_")
+
+with col2:
+    st.markdown("#### 🔴 **Sell Information**")
+    st.markdown(f"**Sell Date**: _{output_data[2].strftime('%Y-%m-%d')}_")
+    st.markdown(f"**Sell Price**: _${output_data[3]:,.2f}_")
+
+# Display a simple price trend chart
+st.markdown("---")
+st.write("### 📉 Price Trend Around Buy/Sell Dates")
+price_data = get_dataFrame(selected_ticker)
+# st.write(price_data.head())
+st.line_chart(price_data['Close'])
+fig = charts.plot_trading_signal(price_data)
+st.plotly_chart(fig)
---
+++ deployment/charts.py
@@ -0,0 +1,134 @@
+import pandas as pd
+from ta.trend import SMAIndicator, EMAIndicator, MACD
+from ta.momentum import RSIIndicator
+import numpy as np
+import plotly.graph_objects as go
+import streamlit as st
+
+
+def get_indicator_signals(data):
+    plot_dt = data.copy()
+    """
+    This function calculates various technical indicators and generates trading signals
+    for a given stock price plot_dtFrame.
+
+    Parameters:
+    plot_dt (pd.plot_dtFrame): plot_dtFrame containing 'Close' price plot_dt.
+
+    Returns:
+    pd.plot_dtFrame: plot_dtFrame with added columns for SMA, EMA, MACD, RSI, and corresponding signals.
+    """
+    # Calculate 20-day and 50-day Simple Moving Averages (SMA) on the Close price
+    plot_dt['SMA_20'] = SMAIndicator(
+        close=plot_dt['Close'], window=20).sma_indicator()
+    plot_dt['SMA_50'] = SMAIndicator(
+        close=plot_dt['Close'], window=50).sma_indicator()
+
+    # Calculate 12-day and 26-day Exponential Moving Averages (EMA) on the Close price
+    plot_dt['EMA_12'] = EMAIndicator(
+        close=plot_dt['Close'], window=12).ema_indicator()
+    plot_dt['EMA_26'] = EMAIndicator(
+        close=plot_dt['Close'], window=26).ema_indicator()
+
+    # 3. Generate Trading Signals Based on SMA and EMA Crossovers
+    plot_dt['SMA_Signal'] = 0
+    plot_dt['EMA_Signal'] = 0
+
+    # Generate SMA signals: 1 for Buy, -1 for Sell
+    plot_dt.loc[(plot_dt['SMA_20'] > plot_dt['SMA_50']),
+                'SMA_Signal'] = 1  # Buy Signal (Golden Cross)
+    plot_dt.loc[(plot_dt['SMA_20'] < plot_dt['SMA_50']), 'SMA_Signal'] = - \
+        1  # Sell Signal (Death Cross)
+
+    # Generate EMA signals: 1 for Buy, -1 for Sell
+    plot_dt.loc[(plot_dt['EMA_12'] > plot_dt['EMA_26']),
+                'EMA_Signal'] = 1  # Buy Signal
+    plot_dt.loc[(plot_dt['EMA_12'] < plot_dt['EMA_26']),
+                'EMA_Signal'] = -1  # Sell Signal
+
+    # 4. Calculate MACD and Signal Line Using ta Library
+    macd = MACD(close=plot_dt['Close'], window_slow=26,
+                window_fast=12, window_sign=9)
+    plot_dt['MACD'] = macd.macd()
+    plot_dt['Signal_Line'] = macd.macd_signal()
+    plot_dt['MACD_Histogram'] = macd.macd_diff()
+
+    # Generate MACD signals based on crossovers
+    plot_dt['MACD_Signal'] = 0
+    plot_dt.loc[plot_dt['MACD'] > plot_dt['Signal_Line'],
+                'MACD_Signal'] = 1  # Buy Signal
+    plot_dt.loc[plot_dt['MACD'] < plot_dt['Signal_Line'],
+                'MACD_Signal'] = -1  # Sell Signal
+
+    # 5. Calculate Relative Strength Index (RSI)
+    rsi = RSIIndicator(close=plot_dt['Close'], window=14)
+    plot_dt['RSI'] = rsi.rsi()
+
+    # Generate Buy and Sell signals based on RSI levels
+    plot_dt['RSI_Signal'] = 0
+    plot_dt.loc[plot_dt['RSI'] > 70, 'RSI_Signal'] = - \
+        1  # Sell Signal (Overbought)
+    # Buy Signal (Oversold)
+    plot_dt.loc[plot_dt['RSI'] < 30, 'RSI_Signal'] = 1
+    plot_dt.dropna(inplace=True)
+    return plot_dt
+
+
+def make_decision(plot_dt):
+    plot_dt['Decision'] = 0  # hold
+    for i in plot_dt.index[1:]:
+        if (plot_dt.loc[i, 'SMA_Signal']
+            + plot_dt.loc[i, 'MACD_Signal']
+                + plot_dt.loc[i, 'RSI_Signal']) >= 2:
+            plot_dt.loc[i, 'Decision'] = 1  # buy
+        elif (plot_dt.loc[i, 'SMA_Signal']+plot_dt.loc[i, 'MACD_Signal']+plot_dt.loc[i, 'RSI_Signal']) <= -2:
+            plot_dt.loc[i, 'Decision'] = 2  # sell
+        else:
+            plot_dt.loc[i, 'Decision'] = 0
+    return plot_dt
+
+
+def plot_candlestick_with_decisions(go_df):
+    """
+    This function generates a candlestick chart with buy and sell trading decisions.
+
+    Parameters:
+    go_df (pd.plot_dtFrame): plot_dtFrame containing 'TradingDate', 'Open', 'High', 'Low', 'Close', and 'Decision' columns.
+                          The 'Decision' column should contain values for trading decisions:
+                          1 for Buy, 2 for Sell, and 0 for Hold.
+
+    Returns:
+    the candlestick chart with buy and sell markers.
+    """
+    index_as_x = go_df.index
+    # Create a candlestick chart
+    fig = go.Figure(data=[go.Candlestick(x=index_as_x,
+                                         open=go_df['Open'],
+                                         high=go_df['High'],
+                                         low=go_df['Low'],
+                                         close=go_df['Close'],
+                                         name='Candlestick'),
+                          go.Scatter(x=index_as_x[go_df['Decision'] == 1],
+                                     y=go_df['Close'][go_df['Decision'] == 1],
+                                     mode='markers',
+                                     marker=dict(color='green', size=10),
+                                     name='Buy Decision'),
+                          go.Scatter(x=index_as_x[go_df['Decision'] == 2],
+                                     y=go_df['Close'][go_df['Decision'] == 2],
+                                     mode='markers',
+                                     marker=dict(color='red', size=10),
+                                     name='Sell Decision')])
+
+    # Update layout for better visualization
+    fig.update_layout(title='Candlestick Chart with Trading Decisions',
+                      xaxis_title='TradingDate',
+                      yaxis_title='Price',
+                      xaxis_rangeslider_visible=False)
+
+    return fig
+
+
+def plot_trading_signal(plot_dt):
+    plot_dt = get_indicator_signals(plot_dt)
+    plot_dt = make_decision(plot_dt)
+    return plot_candlestick_with_decisions(plot_dt)
